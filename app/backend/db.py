b57859fd4b34317d4ca5662b980675b232c13923
vimex1 <154634383+vimex1@users.noreply.github.com>
2025-08-15 16:47:43 +0300

    Reworked migrations to async version

--- app/backend/db.py
+++ app/backend/db.py
@@ -1,13 +1,14 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
 
-URL_DATABASE = "postgresql://postgres:postgres@localhost:5432/ernest_db"
+URL_DATABASE = "postgresql+asyncpg://postgres:postgres@localhost:5432/ernest_db"
 
-engine = create_engine(URL_DATABASE)
+engine = create_async_engine(URL_DATABASE)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+async_session_maker = async_sessionmaker(
+    engine, expire_on_commit=False, class_=AsyncSession
+)
 
 
 class Base(DeclarativeBase):
     pass
-
