88cda582851bc054faf320b3986d9ef74189a027
vimex1 <154634383+vimex1@users.noreply.github.com>
2025-08-15 16:51:20 +0300

    Make async db session

--- app/backend/db_depends.py
+++ app/backend/db_depends.py
@@ -1,11 +1,9 @@
-from app.backend.db import SessionLocal
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.backend.db import async_session_maker
 
 
-async def get_db():
-    """Получение подключения к базе данных"""
-    db = SessionLocal()
-    try:
-        yield db  
-    finally:
-        db.close()
-        
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
