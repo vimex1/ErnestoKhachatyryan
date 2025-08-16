4ea6ff239e0bfd06c30bd13534c94eff6c2ebc73
vimex1 <154634383+vimex1@users.noreply.github.com>
2025-08-16 14:42:15 +0300

    HTTP Basic Auth added

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
 from app.routers import category
 from app.routers import products
+from app.routers import auth
 
 app = FastAPI()
 
 
 @app.get('/')
 async def welcome() -> dict:
-    return {'message': 'Добро пожаловать в магазин'}
+    return {'message': 'Добро пожаловать в магазин Ernesto Khachatyryan'}
 
 app.include_router(category.router)
 app.include_router(products.router)
+app.include_router(auth.router)
