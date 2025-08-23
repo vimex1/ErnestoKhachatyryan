350a22f0addf29bdb5cd48b4d124abd8619c87d4
vimex1 <154634383+vimex1@users.noreply.github.com>
2025-08-24 01:20:41 +0300

    Logging now has levels

--- app/backend/config.py
+++ app/backend/config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from pathlib import Path
 
 load_dotenv()
 
@@ -7,3 +8,14 @@ URL_DATABASE = os.getenv("URL_DATABASE")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
+
+
+LOG_DIR = Path('logs')
+LOG_DIR.mkdir(parents=True, exist_ok=True)
+
+LOG_CONF = {
+    "format": "Log: [{extra[log_id]}:{time} - {level} - {message} ",
+    "enqueue": True,
+    "rotation": "10 MB",
+    "retention": "10 days"
+}
--- app/log.py
+++ app/log.py
@@ -2,14 +2,25 @@ from uuid import uuid4
 from fastapi import Request
 from fastapi.responses import JSONResponse
 from loguru import logger
+from app.backend.config import LOG_CONF, LOG_DIR
+
 
 logger.add(
-    "info.log",
-    format="Log: [{extra[log_id]}:{time} - {level} - {message} ",
-    level="INFO",
-    enqueue=True,
-    rotation="10 MB",
-    retention="10 days"
+        LOG_DIR / "info.log",
+        level="INFO",
+        **LOG_CONF,
+    )
+
+logger.add(
+        LOG_DIR / "warning.log",
+        level="WARNING",
+        **LOG_CONF,
+    )
+
+logger.add(
+    LOG_DIR / "error.log",
+    level="ERROR",
+    **LOG_CONF,
 )
 
 
@@ -18,11 +29,15 @@ async def log_middleware(request: Request, call_next):
     with logger.contextualize(log_id=log_id):
         try:
             response = await call_next(request)
+
             if response.status_code in [401, 402, 403, 404]:
                 logger.warning(f"Request to {request.url.path} failed")
+                
             else:
                 logger.info("Successfully accessed " + request.url.path)
+        
         except Exception as ex:
             logger.error(f"Request to {request.url.path} failed: {ex}")
             response = JSONResponse(content={"success": False}, status_code=500)
+
         return response
